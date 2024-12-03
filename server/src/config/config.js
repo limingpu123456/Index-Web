cda19572eaa43c64f5990f7b19bf740a0bc6cdcd
limingpu <85180586@qq.com>
2024-12-03 17:54:22 +0900

    2

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -6,20 +6,20 @@ module.exports = {
   redisConfig: {
     host: "localhost",
     port: "6379",
-    password: "123456",
+    password: "",
     db: 2,
   },
   // MySQL 配置
   dbConfig: {
     database: "yuindex",
     username: "root",
-    password: "123456",
+    password: "woshizhu123",
     host: "localhost",
     port: 3306,
   },
   // 百度翻译配置，自行申请免费版即可（https://fanyi-api.baidu.com/）
   baiduFanYiConfig: {
-    appid: "",
-    key: "",
+    appid: "20241203002218017",
+    key: "krffhJ1nzLGyBmazbg6Y",
   },
 };
